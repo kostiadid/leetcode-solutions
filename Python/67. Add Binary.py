168cfee6098d821687c7f62b7a9db6d5324dd2cd
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2025-10-19 20:17:02 -0300

    Add binary addition function in Add Binary.py
    Implement a function to add two binary strings.

---
+++ Python/67. Add Binary.py
@@ -0,0 +1,36 @@
+# Given two binary strings a and b, return their sum as a binary string.
+
+ 
+
+# Example 1:
+
+# Input: a = "11", b = "1"
+# Output: "100"
+# Example 2:
+
+# Input: a = "1010", b = "1011"
+# Output: "10101"
+ 
+
+# Constraints:
+
+# 1 <= a.length, b.length <= 104
+# a and b consist only of '0' or '1' characters.
+# Each string does not contain leading zeros except for the zero itself.
+
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        aInt = int(a,2)
+        bInt = int(b,2)
+        abInt = bInt + aInt
+        abBin = bin(abInt)[2:]
+        return str(abBin)
+
+
+
+
+
+
+
+
