ac513625300de67ce10f41a5186637a4b3c67134
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-03 02:09:06 -0400

    Update substrings-of-size-three-with-distinct-characters.py

--- Python/substrings-of-size-three-with-distinct-characters.py
+++ Python/substrings-of-size-three-with-distinct-characters.py
@@ -8,8 +8,9 @@ class Solution:
         for i in range(len(s)):
             my_dict[s[i]] += 1
             if i - left == 3:
-                my_dict[s[left]] -=1
-                if  my_dict[s[left]] == 0: del  my_dict[s[left]] 
+                remove_el = s[left]
+                my_dict[remove_el] -=1
+                if  my_dict[remove_el] == 0: del  my_dict[remove_el] 
                 left+=1
             if len(my_dict) == 3:
                 counter+=1
