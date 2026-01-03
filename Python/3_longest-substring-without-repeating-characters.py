488f618e0cdc40f46815b511d40f24bc1ad9b030
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-02 21:53:01 -0400

    Update 3_longest-substring-without-repeating-characters.py

--- Python/3_longest-substring-without-repeating-characters.py
+++ Python/3_longest-substring-without-repeating-characters.py
@@ -1,18 +1,23 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         hash_table = {}
-        for  i in  s:
-            if  i not in  hash_table:
-                hash_table[i]=i
-        counter = 0
         counter_list = []
+        counter = 0
         for i in s:
             if i in hash_table:
-                counter+=1
-                del hash_table[i]
-            else: 
                 counter_list.append(counter)
-                continue
+                keys_to_delete = []
+                for key in hash_table:
+                    keys_to_delete.append(key)
+                    if key == i:
+                        break
+                for key in keys_to_delete:
+                    del hash_table[key]
+                    continue
+                counter = len(hash_table)
+            hash_table[i]=i
+            counter+=1
+        if counter > 0:
             counter_list.append(counter)
-        big_length = sorted(counter_list)[-1]
+        big_length = sorted(counter_list)[-1] if counter_list else 0
         return big_length
