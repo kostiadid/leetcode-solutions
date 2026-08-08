3c392dc5c3aa9d2a3aa8e8067118181e47653f0a
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-08-08 12:40:20 +0200

    Refactor palindrome check method
    Refactored palindrome check to use two-pointer technique for improved efficiency.

--- CSharp/Check_Palindrome_by_Filtering_Non_Letters.cs
+++ CSharp/Check_Palindrome_by_Filtering_Non_Letters.cs
@@ -26,25 +26,40 @@ class Result
 
     public static bool isAlphabeticPalindrome(string code)
     {
-        var strOnly = new string(code.Where(x=>char.IsLetter(x)).ToArray()).ToLower();
-        bool flag = false;
-        var mid = strOnly.Length / 2;
-        if(strOnly.Length % 2 == 0){
-            string result = new string(strOnly.Substring(mid).Reverse().ToArray());
 
-            if( strOnly.Substring(0,mid) == result){
-               return true ;
-            }
-            
-        } else {
-            var midl =  (strOnly.Length/2);
-            string res = new string(strOnly.Substring(midl + 1).Reverse().ToArray());
-            if( strOnly.Substring(0,midl) == res){
-               return true ;
-            }
+        var strOnly = new string(code.Where(x=>char.IsLetter(x)).ToArray()).ToLower();
+        var l=0;
+        var r =strOnly.Length - 1;
+        while(l<r){
+                if(strOnly[l] != strOnly[r]){
+                  return false;   
+                }
+                l++;
+                r--;
         }
+        return true;
+        // var strOnly = new string(code.Where(x=>char.IsLetter(x)).ToArray()).ToLower();
+        // bool flag = false;
+        // var mid = strOnly.Length / 2;
+        // if(strOnly.Length % 2 == 0){
+        //     string result = new string(strOnly.Substring(mid).Reverse().ToArray());
+
+        //     if( strOnly.Substring(0,mid) == result){
+        //        return true ;
+        //     }
+            
+        // } else {
+        //     var midl =  (strOnly.Length/2);
+        //     string res = new string(strOnly.Substring(midl + 1).Reverse().ToArray());
+        //     if( strOnly.Substring(0,midl) == res){
+        //        return true ;
+        //     }
+        // }
         
-        return false;
+        // return false;
+
+
+        
     }
 
 }
